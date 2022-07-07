df95e795be12896b3b37ed1365dba580f73bc174
Aleksey Poluyanov <56654805+aaplnv@users.noreply.github.com>
2022-07-07 18:50:04 +0300

    Check for necessary items in config

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/jasonlvhit/gocron"
+	"gopkg.in/telebot.v3/layout"
 	"main/cache"
 	"main/telegram"
 	"sync"
@@ -14,18 +15,43 @@ var wg sync.WaitGroup
 func init() {
 	// Set up logging level here
 	log.SetLevel(log.InfoLevel)
+
+	lt, err := layout.NewDefault("settings.yml", "default")
+	if err != nil {
+		log.Panic("Failed to initialize layout: ", err)
+	}
+
+	if lt.Settings().Token == "" {
+		log.Panic("Telegram token is not set")
+	}
+
+	if lt.String("mariadb.host") == "" {
+		log.Panic("Database host is not set")
+	}
+
+	if lt.String("mariadb.login") == "" {
+		log.Panic("Database login is not set")
+	}
+
+	if lt.String("mariadb.password") == "" {
+		log.Panic("Database password is not set")
+	}
+
+	if lt.String("mariadb.port") == "" {
+		log.Panic("Database port is not set")
+	}
 }
 
 func main() {
 	log.Info("Starting application...")
-	cache.UpdateCache()
+	cache.UpdateQuotes()
 
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
 		telegram.StartBot()
 	}()
-	gocron.Every(10).Seconds().Do(cache.UpdateCache)
+	gocron.Every(10).Seconds().Do(cache.UpdateQuotes)
 	go gocron.Start()
 	wg.Wait()
 }
