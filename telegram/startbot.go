62c02f54562a0715005fb9ba3791e92fb95edd47
Aleksey Poluyanov <56654805+aaplnv@users.noreply.github.com>
2022-07-08 17:12:54 +0300

    setAllCurrencies

--- telegram/startbot.go
+++ telegram/startbot.go
@@ -105,6 +105,24 @@ func getUser(id int64) *ent.User {
 	return user
 }
 
+func setAllCurrencies(currencies string, id int64) {
+	lt, err := layout.NewDefault("settings.yml", "default")
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := ent.Open("mysql", fmt.Sprint(lt.String("mariadb.login"), ":", lt.String("mariadb.password"), "@tcp(", lt.String("mariadb.host"), ":", lt.String("mariadb.port"), ")/testone?parseTime=True"))
+	if err != nil {
+		log.Error("Can't connect to MySQL", err)
+	}
+	defer client.Close()
+
+	_, err = client.User.Update().Where(user.TelegramId(id)).SetAllStables(currencies).Save(context.Background())
+
+	if err != nil {
+		log.Error(err)
+	}
+}
+
 func buildPriceRow(currency string, lt *layout.DefaultLayout) string {
 	var price float64
 	switch currency {
